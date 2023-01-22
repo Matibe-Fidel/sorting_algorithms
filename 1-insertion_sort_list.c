2614e1206334c5024da0b591888120969971c244
Matibe-Fidel <djef235@gmail.com>
2023-01-21 17:08:46 -0800

    InsertionSort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,7 +2,7 @@
 
 /**
  * swap_nodes - Swap two nodes in a listint_t doubly-linked list
- * @h: A pointer to the head of the doubly-linked list.
+ * @h: A pointer to the head of the doubly-linked list
  * @n1: A pointer to the first node to swap
  * @n2: The second node to swap
  */
@@ -21,11 +21,12 @@ void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
 	*n1 = n2->prev;
 }
 
+
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers
- * @list: A pointer to the head of a doubly-linked list of integers.
+ * insertion_sort_list - sorts a doubly linked list of
+ * integers in ascending order
+ * @list: A pointer to the head of the doubly linked list
  *
- * Description: Prints the list after each swap.
  */
 void insertion_sort_list(listint_t **list)
 {
@@ -33,7 +34,6 @@ void insertion_sort_list(listint_t **list)
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-
 	for (iter = (*list)->next; iter != NULL; iter = tmp)
 	{
 		tmp = iter->next;
@@ -43,5 +43,8 @@ void insertion_sort_list(listint_t **list)
 			swap_nodes(list, &insert, iter);
 			print_list((const listint_t *)*list);
 		}
+
 	}
+
+
 }
